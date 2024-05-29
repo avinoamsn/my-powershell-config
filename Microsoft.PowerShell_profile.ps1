00f2983aaa18b30c2605f509f023865981587df6
Avi Nerenberg <Avi Nerenberg>
2024-05-28 21:46:18 -0400

    feat(profile): adds `fnm` & sources the `oh-my-posh` config file from `my-powershell-config` dir"

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -17,7 +17,9 @@ Import-Module posh-git
 # ohmyposh init (from: https://www.hanselman.com/blog/my-ultimate-powershell-prompt-with-oh-my-posh-and-the-windows-terminal)
 # oh-my-posh --init --shell pwsh --config ~/atomic.omp.json | Invoke-Expression # ~/jandedobbeleer.omp.json
 # oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/atomic.omp.json" | Invoke-Expression
-oh-my-posh init pwsh --config ~/pwsh10k.omp.json | Invoke-Expression # combination b/w https://github.com/Kudostoy0u/pwsh10k & https://github.com/u0hz/powerlevel10k-oh-my-posh
+oh-my-posh init pwsh --config ~/my-powershell-config/pwsh10k.omp.json | Invoke-Expression # combination b/w https://github.com/Kudostoy0u/pwsh10k & https://github.com/u0hz/powerlevel10k-oh-my-posh
 
 # improved icons
 Import-Module -Name Terminal-Icons
+
+fnm env --use-on-cd | Out-String | Invoke-Expression
